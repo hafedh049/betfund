3d6ae34924e46f1ed7a2f2c2f95fa29432a428b8
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2024-06-08 21:18:39 +0100

    Updates

---
+++ lib/utils/shared.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color lightGreen = Color.fromARGB(255, 186, 244, 0);
+const Color dark = Colors.black;
+const Color transparent = Colors.transparent;
